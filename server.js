aff086bc1484b0e3d82094b669b7250310bc4a99
Zan Hong <mokzanhong92@hotmail.com>
2020-08-05 17:07:17 +0930

    Added connection and database

---
+++ server.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: "3306",
+
+    user: "root",
+
+    password: "mok920322",
+
+    database: "employee_trackerDB"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
+
